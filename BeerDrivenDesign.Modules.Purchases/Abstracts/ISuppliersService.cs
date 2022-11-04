023571becdb15f0956d33b663b2c69e77dfeace1
Alberto Acerbis <alberto.acerbis@gmail.com>
2022-11-04 13:05:24 +0100

    Add Purchases

--- BeerDrivenDesign.Modules.Purchases/Abstracts/ISuppliersService.cs
+++ BeerDrivenDesign.Modules.Purchases/Abstracts/ISuppliersService.cs
@@ -1,6 +1,8 @@
-﻿namespace BeerDrivenDesign.Modules.Purchases.Abstracts;
+﻿using BeerDrivenDesign.Modules.Purchases.CustomTypes;
+
+namespace BeerDrivenDesign.Modules.Purchases.Abstracts;
 
 public interface ISuppliersService
 {
-    
+    Task<bool> VerifySupplierAsync(FornitoreId fornitoreId);
 }
